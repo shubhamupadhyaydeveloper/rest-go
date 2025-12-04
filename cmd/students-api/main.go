50a44974d38507dbdc1822cdded90189bf8e6d58
Shubham Upadhyay <shubhamwork48@gmail.com>
2025-12-04 19:48:22 +0530

    get method implemented

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/shubhamupadhyaydeveloper/students-api/internal/config"
+)
 
 func main(){
-	fmt.Println("learning api backend")
+	// load config
+    cfg := config.Load()
+    
+	// setup Router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to go server created by shubham Upadhyay"))
+	})
+
+	// setup Server
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Address,
+		Handler: router,
+	}
+
+	fmt.Println("Server is running on port", cfg.HTTPServer.PORT)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start the server")
+	}
+
 }
 
---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	PORT int `yaml:"port" env-required:"true"`
+	Address string `yaml:"address" env-required:"true"`
+}
+
+type Config struct {
+	Env string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer `yaml:"http_server" env-required:"true"`
+}
+
+
+func Load() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		configPath = "config/config.yaml"
+	}
+
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("can not read the config file: %s", err.Error())
+	}
+
+	return &cfg
+}
+
+
